2c4995459be808a1b36668e29152b1ca51c9eb5a
Ofosu Osei <goofosuosei@gmail.com>
2024-04-27 11:51:04 -0400

    frontend updates

--- frontend/client.py
+++ frontend/client.py
@@ -3,9 +3,46 @@ import requests
 import json
 
 # Define the URL of the backend API
-API_URL = "http://backend:8083/api/chat"
+API_URL = "http://localhost:8083/api/chat"
 
-st.title('Chat with AI')
+gradient_text_html = """
+<style>
+.gradient-text {
+    font-weight: bold;
+    background: -webkit-linear-gradient(left, red, orange);
+    background: linear-gradient(to right, red, orange);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    display: inline;
+    font-size: 3em;
+}
+</style>
+<div class="gradient-text">simplyChat</div>
+"""
+st.markdown(gradient_text_html, unsafe_allow_html=True)
+
+model = st.radio(
+    "",
+    options=["Llama 3-70B", "GPT-3.5", "Claude-3 Haiku", "Mixtral 8x7B"],
+    index=0,
+    horizontal=True,
+)
+st.session_state["model"] = model
+
+# Handle each model's availability
+if st.session_state["model"] in ["GPT-3.5", "Claude-3 Haiku", "Mixtral 8x7B"]:
+    st.warning(f"🛑 {st.session_state['model']} is still under construction. Please check back later.")
+elif st.session_state["model"] == "Llama 3-70B":
+    st.success(f"✅ {st.session_state['model']} is ready to go! Start your chat now.")
+
+with open("ui/sidebar.md", "r") as sidebar_file:
+    sidebar_content = sidebar_file.read()
+
+with open("ui/styles.md", "r") as styles_file:
+    styles_content = styles_file.read()
+
+st.sidebar.markdown(sidebar_content)
+st.write(styles_content, unsafe_allow_html=True)
 
 # Text input for user prompt
 user_input = st.text_input("Enter your prompt:", "")
---
+++ frontend/ui/sidebar.md
@@ -0,0 +1,7 @@
+# simplyChat
+
+SimplyChat offers an intuitive and user-friendly interface that seamlessly interacts with a Rust-based Large Language Model (LLM) Inference API. 
+
+Simply type in your questions or requests, and SimplyChat will promptly deliver the most accurate and relevant responses to complete your sentences.
+
+
